e46f8e0907e8323c75ebd7e73969e8077035bc9f
milkakeza <m.isingizwe1@alustudent.com>
2025-01-16 01:18:15 +0200

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -21,7 +21,9 @@ def minOperations(n):
         return 0
 
     dp = [0] * (n + 1)
-    for i in range(2, n + 1):
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
         dp[i] = 1 + min(dp[i - 1], dp[i // 2] + i % 2)
 
     return dp[n]
